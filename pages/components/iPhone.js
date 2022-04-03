0ac7bfad2b85870c993cc9755cc1b1f03c2ebf88
schikin <sergey.chikin@toptal.com>
2022-04-03 14:55:29 +0400

    Initial commit

---
+++ pages/components/iPhone.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+
+class iPhone extends React.Component {
+  render() {
+    return (
+      <div className="marvel-device iphone-x">
+        <div className="notch">
+          <div className="camera"></div>
+          <div className="speaker"></div>
+        </div>
+        <div className="top-bar"></div>
+        <div className="sleep"></div>
+        <div className="bottom-bar"></div>
+        <div className="volume"></div>
+        <div className="overflow">
+          <div className="shadow shadow--tr"></div>
+          <div className="shadow shadow--tl"></div>
+          <div className="shadow shadow--br"></div>
+          <div className="shadow shadow--bl"></div>
+        </div>
+        <div className="inner-shadow"></div>
+        <div className="screen">
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default iPhone;
